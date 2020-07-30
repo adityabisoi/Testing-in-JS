c1fedfa1a9aa2ec7cedd2b0669d758082cfc815f
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-07-30 22:06:11 +0530

    integration test added

--- util.test.js
+++ util.test.js
@@ -1,5 +1,9 @@
-const { generateText } = require('./util')
+const { generateText, checkAndGenerate } = require('./util')
 
 test('output',()=>{
     expect(generateText('Adi', 22)).toBe('Adi (22 years old)')
+})
+
+test('output', () => {
+    expect(checkAndGenerate('Adi', 22)).toBe('Adi (22 years old)')
 })
